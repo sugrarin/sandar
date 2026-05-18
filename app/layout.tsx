47b2d69eaf4d7ecd3d41f285940a372b2da11c7c
Timur Sugrarin <86412452+sugrarin@users.noreply.github.com>
2026-05-18 11:19:37 +0500

    Replace PT Mono with Inter font from Google Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,16 @@ export default function RootLayout({
   return (
     <html lang="ru" suppressHydrationWarning>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
         <link
           rel="icon"
           type="image/png"
